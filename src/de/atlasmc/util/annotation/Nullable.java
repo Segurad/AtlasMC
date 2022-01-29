db194b92e4c6498dc88e4043e8e2216335f1385e
Segurad <nomendo@gmx.de>
2022-01-29 23:50:12 +0100

    Improved ThreadSafe documentation + added Nullable

--- src/de/atlasmc/util/annotation/Nullable.java
+++ src/de/atlasmc/util/annotation/Nullable.java
@@ -5,12 +5,14 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
 /**
- * Types, Methods, Fields and Constructors marked with this annotation should be assumed and implemented as ThreadSafe.
+ * Method with this annotation may return null
+ * Parameters and fields with this annotation may be null
  */
 @Documented
 @Retention(RetentionPolicy.CLASS)
-@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.CONSTRUCTOR})
-public @interface ThreadSafe {
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
+public @interface Nullable {
 
 }
