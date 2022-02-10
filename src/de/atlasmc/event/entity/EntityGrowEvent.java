6867159bd939fc81f22ab28ebfe6406b677d645e
Segurad <nomendo@gmx.de>
2022-02-10 02:20:15 +0100

    new EntityGrowEvent

---
+++ src/de/atlasmc/event/entity/EntityGrowEvent.java
@@ -0,0 +1,41 @@
+package de.atlasmc.event.entity;
+
+import de.atlasmc.entity.AgeableMob;
+import de.atlasmc.entity.Entity;
+import de.atlasmc.event.Cancellable;
+import de.atlasmc.event.ServerHandlerList;
+
+/**
+ * Called when a {@link AgeableMob} turns into a adult through the age value.<br>
+ * If the event is cancelled the age value will remain at 0 but the Entity is kept as baby.
+ */
+public class EntityGrowEvent extends EntityEvent implements Cancellable {
+
+	private static final ServerHandlerList HANDLERS = new ServerHandlerList();
+
+	private boolean cancelled;
+	
+	public EntityGrowEvent(Entity entity) {
+		super(entity);
+	}
+
+	@Override
+	public void setCancelled(boolean cancelled) {
+		this.cancelled = cancelled;		
+	}
+
+	@Override
+	public boolean isCancelled() {
+		return cancelled;
+	}
+
+	@Override
+	public ServerHandlerList getHandlers() {
+		return HANDLERS;
+	}
+	
+	public static ServerHandlerList getHandlerList() {
+		return HANDLERS;
+	}
+
+}
