62d519cdba1f9b33d797ed476a8f3528840fcd29
Segurad <nomendo@gmx.de>
2022-04-10 22:04:37 +0200

    Implemented EntityEquipment interface (required for some mobs)

---
+++ src/de/atlascore/inventory/EntityEquipment.java
@@ -0,0 +1,63 @@
+package de.atlascore.inventory;
+
+import de.atlasmc.inventory.ItemStack;
+
+public interface EntityEquipment {
+
+	public void setBootsDropChance(float chance);
+
+	public void setLeggingsDropChance(float chance);
+
+	public void setChestplateDropChance(float chance);
+
+	public void setHelmetDropChance(float chance);
+
+	public void setBoots(ItemStack item);
+
+	public void setLeggings(ItemStack item);
+
+	public void setChestplate(ItemStack item);
+
+	public void setHelmet(ItemStack item);
+
+	public void setOffHandChance(float chance);
+
+	public void setMainHandDropChance(float chance);
+
+	public void setMainHand(ItemStack item);
+
+	public void setOffHand(ItemStack item);
+
+	public boolean hasArmorDropChance();
+
+	public boolean hasArmor();
+
+	public boolean hasHandItemDropChance();
+
+	public boolean hasHandItem();
+
+	public float getBootsDropChance();
+
+	public float getChestplateDropChance();
+
+	public float getLeggingsDropChance();
+
+	public float getHelmetDropChance();
+
+	public ItemStack getBoots();
+	
+	public ItemStack getLeggings();
+	
+	public ItemStack getChestplate();
+	
+	public ItemStack getHelmet();
+	
+	public float getMainHandDropChance();
+	
+	public float getOffHandDropChance();
+	
+	public ItemStack getMainHand();
+	
+	public ItemStack getOffHand();
+
+}
