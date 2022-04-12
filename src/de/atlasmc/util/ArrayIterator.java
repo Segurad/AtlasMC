5dd4a8795ecd1204a05ff87988786d19f5cb2316
Segurad <nomendo@gmx.de>
2022-04-12 12:45:41 +0200

    New Iterator implementation for arrays

---
+++ src/de/atlasmc/util/ArrayIterator.java
@@ -0,0 +1,41 @@
+package de.atlasmc.util;
+
+import java.util.Iterator;
+
+public class ArrayIterator<E> implements Iterator<E> {
+
+	private final E[] array;
+	private int index;
+	private final boolean canRemove;
+	
+	public ArrayIterator(E[] array, boolean canRemove) {
+		this.array = array;
+		this.canRemove = canRemove;
+	}
+	
+	@Override
+	public boolean hasNext() {
+		for (int i = index+1; i < array.length; i++)
+			if (array[i] != null)
+				return true;
+		return false;
+	}
+
+	@Override
+	public E next() {
+		index++;
+		for (; index < array.length; index++)
+			if (array[index] != null)
+				return array[index];
+		return null;
+	}
+
+	@Override
+	public void remove() {
+		if (canRemove)
+			array[index] = null;
+		else
+			Iterator.super.remove();
+	}
+	
+}
