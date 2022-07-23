898a7f1ae629692584de2bae326c226b4ac1c89b
Segurad <nomendo@gmx.de>
2022-07-23 21:09:01 +0200

    Formating and new getBlockDataUnsafe method

--- src/de/atlasmc/block/Block.java
+++ src/de/atlasmc/block/Block.java
@@ -2,19 +2,39 @@ package de.atlasmc.block;
 
 import de.atlasmc.Material;
 import de.atlasmc.block.data.BlockData;
+import de.atlasmc.world.ChunkSection;
 import de.atlasmc.world.EnumBiome;
 import de.atlasmc.world.World;
 
 public interface Block {
 
 	public Material getType();
+	
 	public void setBlockData(BlockData data);
+	
 	public void setType(Material material);
+	
+	/**
+	 * Returns a copy of the {@link BlockData} at the Blocks position
+	 * @return BlockData
+	 */
 	public BlockData getBlockData();
+	
+	/**
+	 * Returns the {@link BlockData} at the Block position<br>
+	 * The returned BlockData is <b>NOT</b> a copy and changes will modify the palette of the {@link ChunkSection}
+	 * @return BlockData
+	 */
+	public BlockData getBlockDataUnsafe();
+	
 	public int getY();
+	
 	public int getX();
+	
 	public int getZ();
+	
 	public EnumBiome getBiome();
+	
 	public World getWorld();
 	
 }
