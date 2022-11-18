53127f8404692804bf64f1693ba082f05b72c7ff
Segurad <nomendo@gmx.de>
2022-11-18 23:34:00 +0100

    Added #equals and #hashCode methods

--- src/de/atlasmc/util/Pair.java
+++ src/de/atlasmc/util/Pair.java
@@ -21,4 +21,35 @@ public final class Pair<A, B> {
 	public static <A, B> Pair<A, B> of(A value1, B value2) {
 		return new Pair<A, B>(value1, value2);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((value1 == null) ? 0 : value1.hashCode());
+		result = prime * result + ((value2 == null) ? 0 : value2.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pair<?, ?> other = (Pair<?, ?>) obj;
+		if (value1 == null) {
+			if (other.value1 != null)
+				return false;
+		} else if (!value1.equals(other.value1))
+			return false;
+		if (value2 == null) {
+			if (other.value2 != null)
+				return false;
+		} else if (!value2.equals(other.value2))
+			return false;
+		return true;
+	}
 }
--- src/de/atlasmc/util/Triple.java
+++ src/de/atlasmc/util/Triple.java
@@ -24,4 +24,41 @@ public final class Triple<A,B,C> {
 		return value3;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((value1 == null) ? 0 : value1.hashCode());
+		result = prime * result + ((value2 == null) ? 0 : value2.hashCode());
+		result = prime * result + ((value3 == null) ? 0 : value3.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Triple<?, ?, ?> other = (Triple<?, ?, ?>) obj;
+		if (value1 == null) {
+			if (other.value1 != null)
+				return false;
+		} else if (!value1.equals(other.value1))
+			return false;
+		if (value2 == null) {
+			if (other.value2 != null)
+				return false;
+		} else if (!value2.equals(other.value2))
+			return false;
+		if (value3 == null) {
+			if (other.value3 != null)
+				return false;
+		} else if (!value3.equals(other.value3))
+			return false;
+		return true;
+	}
+
 }
