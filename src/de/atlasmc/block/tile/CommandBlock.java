2915aeedc8218b3ed5ca8136d16b110cb9fa8057
Segurad <nomendo@gmx.de>
2023-01-16 16:12:33 +0100

    added caching to enum values

--- src/de/atlasmc/block/tile/CommandBlock.java
+++ src/de/atlasmc/block/tile/CommandBlock.java
@@ -1,5 +1,7 @@
 package de.atlasmc.block.tile;
 
+import java.util.List;
+
 import de.atlasmc.Nameable;
 import de.atlasmc.chat.Chat;
 
@@ -42,9 +44,34 @@ public interface CommandBlock extends TileEntity, Nameable {
 		AUTO,
 		REDSTONE;
 
-		public static Mode getByID(int mode) {
-			return values()[mode];
+		private static List<Mode> VALUES;
+		
+		public int getID() {
+			return ordinal();
 		}
+		
+		public static Mode getByID(int id) {
+			return getValues().get(id);
+		}
+		
+		/**
+		 * Returns a immutable List of all Types.<br>
+		 * This method avoid allocation of a new array not like {@link #values()}.
+		 * @return list
+		 */
+		public static List<Mode> getValues() {
+			if (VALUES == null)
+				VALUES = List.of(values());
+			return VALUES;
+		}
+		
+		/**
+		 * Releases the system resources used from the values cache
+		 */
+		public static void freeValues() {
+			VALUES = null;
+		}
+
 	}
 
 }
