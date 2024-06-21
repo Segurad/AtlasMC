befc54e485b37cc9592d5baba4a1fd89cfde6035
Segurad <nomendo@gmx.de>
2024-06-21 13:30:45 +0200

    removed toFile conversion

--- atlas-node-api/src/main/java/de/atlasmc/util/DeleteFileVisitor.java
+++ atlas-node-api/src/main/java/de/atlasmc/util/DeleteFileVisitor.java
@@ -3,6 +3,7 @@ package de.atlasmc.util;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -15,13 +16,13 @@ public class DeleteFileVisitor extends SimpleFileVisitor<Path> {
 	
 	@Override
 	public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-		file.toFile().delete();
+		Files.delete(file);
 		return FileVisitResult.CONTINUE;
 	}
 	
 	@Override
 	public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-		dir.toFile().delete();
+		Files.delete(dir);
 		return FileVisitResult.CONTINUE;
 	}
 
