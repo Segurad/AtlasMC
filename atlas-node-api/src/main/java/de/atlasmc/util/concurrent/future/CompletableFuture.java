5bc1a8b2df70ee81541469960c1732a2e08aea4f
Segurad <nomendo@gmx.de>
2024-06-29 15:17:41 +0200

    added cum future made abstract future

--- atlas-node-api/src/main/java/de/atlasmc/util/concurrent/future/CompletableFuture.java
+++ atlas-node-api/src/main/java/de/atlasmc/util/concurrent/future/CompletableFuture.java
@@ -1,163 +1,16 @@
 package de.atlasmc.util.concurrent.future;
 
-import java.lang.reflect.Array;
-import java.util.concurrent.CopyOnWriteArraySet;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-
-public class CompletableFuture<V> implements Future<V> {
-
-	protected volatile boolean done;
-	protected volatile V result;
-	protected volatile Throwable cause;
-	protected volatile Object listener;
+public class CompletableFuture<V> extends AbstractFuture<V> {
 	
 	@Override
-	public boolean cancel(boolean mayInterruptIfRunning) {
-		return false;
-	}
-
-	@Override
-	public boolean isCancelled() {
-		return false;
-	}
-	
-	@Override
-	public boolean isCancellable() {
-		return false;	
-	}
-
-	@Override
-	public boolean isDone() {
-		return result != null;
-	}
-
-	@Override
-	public V get() throws InterruptedException, ExecutionException {
-		if (isDone())
-			return result;
-		synchronized (this) {
-			if (isDone())
-				return result;
-			wait();
-			Throwable cause = this.cause;
-			if (cause != null)
-				throw new ExecutionException(cause);
-			return result;
-		}
-	}
-
-	@Override
-	public V get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-		if (isDone())
-			return result;
-		synchronized (this) {
-			if (isDone())
-				return result;
-			int nanos = 0;
-			if (unit.ordinal() < TimeUnit.MILLISECONDS.ordinal()) {
-				long val = TimeUnit.NANOSECONDS.convert(nanos, unit);
-				nanos = (int) (val % 1000000);
-			};
-			wait(TimeUnit.MILLISECONDS.convert(timeout, unit), nanos);
-			Throwable cause = this.cause;
-			if (cause != null)
-				throw new ExecutionException(cause);
-			return result;
-		}
-	}
-
-	@Override
-	public V getNow() {
-		return result;
-	}
-
-	@Override
-	public Throwable cause() {
-		return cause;
-	}
-
-	@Override
-	public boolean isSuccess() {
-		return result != null && cause == null && !isCancelled();
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public void setListener(FutureListener<V> listener) {
-		if (isDone()) {
-			listener.complete(this);
-			return;
-		}
-		synchronized (this) {
-			if (isDone()) {
-				listener.complete(this);
-				return;
-			}
-			Object currentListener = this.listener;
-			if (currentListener == null) {
-				this.listener = listener;
-			} else if (currentListener instanceof Array) {
-				CopyOnWriteArraySet<Object> ary =  (CopyOnWriteArraySet<Object>) currentListener;
-				ary.add(listener);
-			} else {
-				CopyOnWriteArraySet<FutureListener<V>> ary = new CopyOnWriteArraySet<>();
-				ary.add((FutureListener<V>) currentListener);
-				ary.add(listener);
-				this.listener = ary;
-			}
-		}
-	}
-	
-	@Override
-	public void removeListener(FutureListener<V> listener) {
-		if (listener == null)
-			return;
-		if (isDone())
-			return;
-		synchronized (this) {
-			if (isDone())
-				return;
-			Object currentListener = this.listener;
-			if (currentListener == null) {
-				return;
-			} if (currentListener instanceof CopyOnWriteArraySet) {
-				@SuppressWarnings("unchecked")
-				CopyOnWriteArraySet<Object> ary = (CopyOnWriteArraySet<Object>) currentListener;
-				ary.remove(listener);
-			} else if (currentListener == listener)
-				this.listener = null;
-		}
-	}
-	
 	public void finish(V result) {
-		finish(result, null);
+		super.finish(result, null);
 	}
 	
-	@SuppressWarnings("unchecked")
+	@Override
 	public void finish(V result, Throwable cause) {
-		if (isDone())
-			throw new IllegalStateException("Already complete");
-		synchronized (this) {
-			if (isDone())
-				throw new IllegalStateException("Already complete");
-			this.result = result;
-			this.cause = cause;
-			this.done = true;
-			Object listener = this.listener;
-			if (listener != null) {
-				if (listener instanceof CopyOnWriteArraySet) {
-					for (FutureListener<V> l : (Iterable<FutureListener<V>>)listener) {
-						l.complete(this);
-					}
-				} else {
-					((FutureListener<V>) listener).complete(this);
-				}
-				this.listener = null;
-			}
-			notifyAll();
-		}
+		super.finish(result, cause);
 	}
 
+	
 }
