ddcc5285b97711c220432daac6508668103d0456
Segurad <nomendo@gmx.de>
2025-09-27 01:42:42 +0200

    removed netty kqueue

--- atlas-common-api/src/main/java/de/atlasmc/io/socket/SocketConfig.java
+++ atlas-common-api/src/main/java/de/atlasmc/io/socket/SocketConfig.java
@@ -16,10 +16,6 @@ import io.netty.channel.epoll.Epoll;
 import io.netty.channel.epoll.EpollIoHandler;
 import io.netty.channel.epoll.EpollServerSocketChannel;
 import io.netty.channel.epoll.EpollSocketChannel;
-import io.netty.channel.kqueue.KQueue;
-import io.netty.channel.kqueue.KQueueIoHandler;
-import io.netty.channel.kqueue.KQueueServerSocketChannel;
-import io.netty.channel.kqueue.KQueueSocketChannel;
 import io.netty.channel.nio.NioIoHandler;
 import io.netty.channel.socket.ServerSocketChannel;
 import io.netty.channel.socket.SocketChannel;
@@ -125,7 +121,6 @@ public class SocketConfig implements ConfigurationSerializable {
 		
 		IO_URING(IoUringServerSocketChannel.class, IoUringSocketChannel.class, IoUringIoHandler.newFactory()),
 		EPOLL(EpollServerSocketChannel.class, EpollSocketChannel.class, EpollIoHandler.newFactory()),
-		K_QUEUE(KQueueServerSocketChannel.class, KQueueSocketChannel.class, KQueueIoHandler.newFactory()),
 		NIO(NioServerSocketChannel.class, NioSocketChannel.class, NioIoHandler.newFactory());
 		
 		private final Class<? extends ServerSocketChannel> serverSocektChannelClass;
@@ -152,10 +147,10 @@ public class SocketConfig implements ConfigurationSerializable {
 				return IO_URING.isAvailable();
 			case EPOLL:
 				return Epoll.isAvailable();
-			case K_QUEUE:
-				return KQueue.isAvailable();
-			default:
+			case NIO:
 				return true;
+			default:
+				return false;
 			}
 		}
 		
